244f30a85b860fac0a643d261fd19d17c8f12bfa
Ulrika L <u.lifvenborg@outlook.com>
2021-08-18 09:16:30 +0200

    Started Vue router and store

---
+++ frontend/beertaster/src/store.js
@@ -0,0 +1,20 @@
+import { createStore } from 'vuex'
+
+const state = {
+    
+}
+
+
+const mutations = {
+
+    
+
+}
+
+const actions = {
+
+
+}
+
+
+export default createStore({state, mutations, actions})
