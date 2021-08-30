30f59bd9b0f1d3b6d3ba1279f59cb1a7c60bbef7
Ulrika L <u.lifvenborg@outlook.com>
2021-08-30 05:31:22 +0200

    Updated router and store

--- frontend/beertaster/src/router.js
+++ frontend/beertaster/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '/src/components/Home.vue'
+import UserPage from '/src/components/UserPage.vue'
 import CreateGrade from '/src/components/create-grade.vue'
 import ViewGrade from '/src/components/view-grades.vue'
 
@@ -9,17 +10,22 @@ import ViewGrade from '/src/components/view-grades.vue'
 const routes = [
     {
         path: '/',
-        name: 'home',
+        name: 'Home',
         component: Home
     },
     {
+        path: '/user-page',
+        name: 'UserPage',
+        component: UserPage
+    },
+    {
         path: '/create-grade',
-        name: 'create-grade',
+        name: 'CreateGrade',
         component: CreateGrade
     },
     {
         path: '/view-grade',
-        name: 'view-grade',
+        name: 'ViewGrade',
         component: ViewGrade
     },
 
