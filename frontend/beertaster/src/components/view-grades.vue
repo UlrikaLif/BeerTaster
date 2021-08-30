a5bc6082636497d1edfff6ce3f486d1e0c59f059
Ulrika L <u.lifvenborg@outlook.com>
2021-08-30 05:37:57 +0200

    started view-grades

---
+++ frontend/beertaster/src/components/view-grades.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Mina omdömen</h1>
+
+       
+    </div>
+    
+
+</template>
+
+<script>
+export default {
+    name: "ViewGrade",
+
+    
+    data(){
+        return{
+
+        }
+    },
+
+    
+    computed:{
+        
+        myGrades(){
+            return this.$store.state.myGrades;
+            
+        }
+    
+
+    },
+
+
+    methods:{
+
+
+    }
+}
+</script>
+
+<style>
+
+</style>
