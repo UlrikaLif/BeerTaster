763fde42b178dfa034255fac3fbb17ae890e6384
Ulrika L <u.lifvenborg@outlook.com>
2021-09-01 19:40:44 +0200

    created Admin view

---
+++ frontend/beertaster/src/components/DeletingBeer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3> Vilket öl vill du radera?</h3>  
+
+        <input type="text" name="beerId" id="beerId">
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name:"DeletingBeer",
+
+}
+</script>
+
+<style>
+
+</style>
